e507fd3295303dbc5545b7d684204ec32a699be3
Daniel Sisson <daniel.sisson@hey.com>
2026-02-17 09:58:14 -0500

    Flatten Framework to pure Unity package structure

--- Assets/Foundry/DesignTools/Runtime/Inspectors/Scripts/LayerAttribute.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-/// <summary>
-/// Attribute to select a single layer.
-/// </summary>
-public class LayerAttribute : PropertyAttribute { }
--- Assets/Foundry/README.md
+++
@@ -1,1 +0,0 @@
-# This is the portion of Foundry that will be unpacked into the Assets directory of projects using it.
